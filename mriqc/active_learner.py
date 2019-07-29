5e71887fdba5b8c76b80875ad1d02e1106d8b26e
dhruv <sharma.1@iitj.ac.in>
2019-07-29 17:36:16 -0400

    prediction after training also

--- mriqc/active_learner.py
+++ mriqc/active_learner.py
@@ -12,19 +12,19 @@ from data_loader import Data
 from strategy import uncertainty_sampling
 from glob import glob
 
-def predict(path):
+def predict(master_path, path):
     '''
     This function forms the engine for the first part of MIQA and AL. This function
     helps in making the predictions for the new data that has been input.
     
     Args:
+        master_path: the absolute path to the master folder with all important directories
+        like training_data.csv, model_weights, log files
         path: path to the csv file with the new input data
     Returns:
         path: the new path to the file with the new input data
     '''
-    # read the variables and data, load the model
-    master_path = '/home/dhruv.sharma/Projects/MRIQC_AL/master_folder'#os.environ['MIQA_AL_PATH']
-    
+    # read the variables and data, load the model    
     weights_dir = 'saved_model'
     model_path = os.path.join(master_path, weights_dir)
     
@@ -56,21 +56,20 @@ def predict(path):
     
     return path
 
-def train(csv_path=None, on_new=False):
+def train(master_path, csv_path=None):
     '''
     This function ins the engine to train the model with the new data just labeled
     by the user of MIQA. The model can also be trained on the previously available
     labeled data.
     
     Args:
+        master_path: the absolute path to the master folder with all important directories
+        like training_data.csv, model_weights, log files
         csv_path: the path to the csv containing the newly labeled data
-        on_new: if True will train on the new data as well, if False will train only
-        on the available training data
     Returns:
         None
     '''
     # read the variables and data, load the model
-    master_path = '/home/dhruv.sharma/Projects/MRIQC_AL/master_folder'#os.environ['MIQA_AL_PATH']
         
     weights_dir = 'saved_model'
     model_path = os.path.join(master_path, weights_dir)
@@ -81,7 +80,7 @@ def train(csv_path=None, on_new=False):
     if not os.path.isdir(model_path):
         os.mkdir(model_path)
                
-    if on_new:
+    if csv_path is not None:
         training_data_path = os.path.join(master_path, 'training_data.csv')
         training_data = Data(training_data_path)
         # load the data
@@ -97,21 +96,28 @@ def train(csv_path=None, on_new=False):
         training_data.add_new_data(query_data)
         
         training_data.save()
-
+    
+    # load the (updated) training data and get the features and labels
     training_data_path = os.path.join(master_path, 'training_data.csv')
     training_data = Data(training_data_path)
     
     _, X, y = training_data.get_feature_and_labels()
     
+    # upload the model and fit over the dataset
     model = Model()
     model.fit(X, y)
     model.save_model(model_path)
     
+    # update the predictions for the new data with the newly trained model
+    if csv_path is not None:
+        return predict(master_path, csv_path)
+    
 if __name__ == '__main__':
+    master_path = '/home/dhruv.sharma/Projects/MRIQC_AL/master_folder'
     csv_path = '/home/dhruv.sharma/Projects/MRIQC_AL/mriqc_output.csv'
     decision_path = '/home/dhruv.sharma/Projects/MRIQC_AL/mriqc_output_decision_dummy.csv'
-#    train(decision_path, on_new=True)
-    predict(csv_path)
+    train(master_path, decision_path)
+#    predict(master_path, csv_path)
     
     
     
