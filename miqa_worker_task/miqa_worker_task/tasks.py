dd3b2a656732d77b61bd8695bef6f455e1edd88b
Matthew Ma <matthew.ma@kitware.com>
2019-08-24 19:44:55 -0400

    Add active learning persisted state

--- miqa_worker_task/miqa_worker_task/tasks.py
+++ miqa_worker_task/miqa_worker_task/tasks.py
@@ -9,6 +9,7 @@ from girder_worker.utils import girder_job
 def retrain_with_data_task(self, csv_file_path):
     from mriqc.active_learner import train
 
+    # the path where the master_folder exists
     master_path = os.environ['MIQA_MRIQC_PATH']
 
     return train(master_path, csv_file_path)
