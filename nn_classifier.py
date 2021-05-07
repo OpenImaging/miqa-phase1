01ec55f7eee5736c6fc67ab72110bb52e4acfc11
Dženan Zukić <dzenan.zukic@kitware.com>
2021-05-07 11:46:03 -0400

    Use 5e-5 learning rate

--- nn_classifier.py
+++ nn_classifier.py
@@ -251,7 +251,7 @@ def train_and_save_model(df, count_train, save_path, num_epochs, val_interval, o
         loss_function = monai.losses.FocalLoss(weight=class_weights, to_onehot_y=True)
     else:
         loss_function = torch.nn.CrossEntropyLoss(weight=class_weights)
-    wandb.config.learning_rate = 1e-4
+    wandb.config.learning_rate = 5e-5
     optimizer = torch.optim.Adam(model.parameters(), wandb.config.learning_rate)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=2)
     wandb.watch(model)
